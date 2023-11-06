97974a6df9348ce7a57144e19ff7917e8712e977
KALKI1804 <145177757+KALKI1804@users.noreply.github.com>
2023-11-06 11:56:40 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,i,f;
+f=i=1;
+printf("Enter a number to find factorial :\n");
+scanf(%d",&n);
+{
+f*=i;
+i++;
+}
+printf("The Factorial of %d is: %d", n,f);
+return 0;
+}
